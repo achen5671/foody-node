bf803be63f29912f44f43b50e8a6b8d7bd7daf87
Andy Chen <44120813+achen5671@users.noreply.github.com>
2023-12-15 01:19:33 -0500

    return user

--- src/api/services/UserService.ts
+++ src/api/services/UserService.ts
@@ -1,9 +1,16 @@
+import { ResourceDoesNotExistError } from "../middlewares/apiErrors";
 import UserRepository from "../repositories/UserRepository";
 
 class UserService {
   login = async (username: string, password: string) => {
     // TODO: use bcrypt to hash password
     const user = await UserRepository.findBy({ username, password });
+
+    if (!user) {
+      throw new ResourceDoesNotExistError("user not found");
+    }
+
+    return user;
   };
 }
 
