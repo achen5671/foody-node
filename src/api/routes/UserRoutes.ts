e10e2e8367d848187857e1508504ff1c0f9e362d
Andy Chen <44120813+achen5671@users.noreply.github.com>
2023-12-17 20:03:24 -0500

    feat: delete weight submission

--- src/api/routes/UserRoutes.ts
+++ src/api/routes/UserRoutes.ts
@@ -107,11 +107,12 @@ class UserRoutes extends BaseRouter {
     );
 
     this.router.get(
-      "/weight-progress/projected/:userId",
+      "/weight-progress/:submissionId/delete",
       this.tryWrapper(async (req: express.Request, res: express.Response) => {
-        const { userId } = req.params;
-        const projected = await UserService.projectedWeightProgress(userId);
-        this.sendSuccessResponse(res, { projected });
+        const { userId } = req;
+        const { submissionId } = req.params;
+        await UserService.deleteWeightSubmission(userId, submissionId);
+        this.sendSuccessResponse(res);
       })
     );
   }
